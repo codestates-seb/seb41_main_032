609670566f35a65644bd8e08e9e6b9d874b9333d
LeeJoongWon <dlwnddnjs48@gmail.com>
2023-01-06 08:58:04 +0900

    상승 하락 표시할 떄 사용하는 삼각형 css

---
+++ client/src/Components/Style/Triangle.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const RedTriangle = styled.span`
+    border-bottom: 5px solid red;
+    border-left: 5px solid transparent;
+    border-right: 5px solid transparent;
+    margin-right: 2px;
+`;
+
+export const BlueTriangle = styled.span`
+    border-top: 5px solid blue;
+    border-left: 5px solid transparent;
+    border-right: 5px solid transparent;
+    margin-right: 2px;
+`;
