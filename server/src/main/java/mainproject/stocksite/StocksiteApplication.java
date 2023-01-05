46f5da1d849e2d81142ec648cee5c7b4cb77d19c
psh1379 <psha1501@gmail.com>
2023-01-05 11:14:52 +0900

    base

---
+++ server/src/main/java/mainproject/stocksite/StocksiteApplication.java
@@ -0,0 +1,13 @@
+package mainproject.stocksite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksiteApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/mainproject/stocksite/StocksiteApplicationTests.java
@@ -0,0 +1,13 @@
+package mainproject.stocksite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
