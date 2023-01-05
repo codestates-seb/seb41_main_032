77cbc7e016f9e239b22f224b03562280de823800
pepsi160ml <111164516+pepsi160ml@users.noreply.github.com>
2023-01-05 16:46:20 +0900

    Revert "feat:맴버crud 구현"
    This reverts commit 5b997d16265fa7d99c7bcd6cb1155f88df300be7.

--- server/src/main/java/mainproject/stocksite/StocksiteApplication.java
+++ server/src/main/java/mainproject/stocksite/StocksiteApplication.java
@@ -1,9 +1,8 @@
 package mainproject.stocksite;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-@EnableJpaAuditing
+
 @SpringBootApplication
 public class StocksiteApplication {
 
--- server/src/main/java/mainproject/stocksite/domain/member/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package mainproject.stocksite.domain.member.repository;
-
-import mainproject.stocksite.domain.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-}
-
