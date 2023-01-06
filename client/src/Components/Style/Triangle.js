f5b95abc5c7d28045f2802efa29beaad202a4120
LeeJoongWon <dlwnddnjs48@gmail.com>
2023-01-06 16:55:19 +0900

    상승,하락 세모 css 수정

--- client/src/Components/Style/Triangle.js
+++ client/src/Components/Style/Triangle.js
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 
 export const RedTriangle = styled.span`
-    border-bottom: 5px solid red;
+    border-bottom: 5px solid #ff4747;
     border-left: 5px solid transparent;
     border-right: 5px solid transparent;
     margin-right: 2px;
 `;
 
 export const BlueTriangle = styled.span`
-    border-top: 5px solid blue;
+    border-top: 5px solid #3a74ff;
     border-left: 5px solid transparent;
     border-right: 5px solid transparent;
     margin-right: 2px;
