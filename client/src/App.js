595bba36374d9501129bbf548025a0ce5a5028ef
FE41PIW <eatdrink0507@gmail.com>
2023-01-06 16:35:57 +0900

    feat : Sidebar CSS 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-import Header from "./Page/Global/Header/index";
+import styled from "styled-components";
 
+import Header from "./Page/Global/Header/index";
+import Sidebar from "./Page/Global/Sidebar/index";
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 function App() {
   return (
     <div className="App">
       <Header />
+      <Div>
+        <Sidebar />
+      </Div>
     </div>
   );
 }
