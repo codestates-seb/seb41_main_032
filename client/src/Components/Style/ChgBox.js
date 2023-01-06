8c8303edc4af505370fa0b7b2692261621340ffc
LeeJoongWon <dlwnddnjs48@gmail.com>
2023-01-06 16:54:58 +0900

    Blue,Red 박스(등락률) css

---
+++ client/src/Components/Style/ChgBox.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const BlueBox = styled.span`
+    background-color: #097df3;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 20px;
+    display: inline-block;
+    min-width: 57px;
+    margin-top: 4px;
+    border-radius: 3px;
+    color: #fff;
+    text-align: center;
+    padding: 5px 8px;
+`;
+
+export const RedBox = styled.span`
+    background-color: #ed3738;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 20px;
+    display: inline-block;
+    min-width: 57px;
+    margin-top: 4px;
+    border-radius: 3px;
+    color: #fff;
+    text-align: center;
+    padding: 5px 8px;
+`;
