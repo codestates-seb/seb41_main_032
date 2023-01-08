7e0c4e963430e52a83815897d8658f04255dadea
LeeJoongWon <dlwnddnjs48@gmail.com>
2023-01-09 01:37:26 +0900

    임시 프록시 서버 구축

---
+++ client/ProxyServer/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+require('dotenv').config();
+const errorHandler = require('./middleware/error');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// Rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 Mins
+    max: 600,
+});
+app.use(limiter);
+app.set('trust proxy', 1);
+
+// Enable cors
+app.use(cors());
+
+// Routes
+app.use('/api', require('./routes'));
+
+// Error handler middleware
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
