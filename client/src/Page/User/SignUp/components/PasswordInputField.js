6b01eb10312a0343cf6bb07543da4e17c33cf869
yan9vvoojin <yan9vvoojin@naver.com>
2023-01-08 12:24:17 +0900

    refactor: 비밀번호 인풋 영역 수정

--- client/src/Page/User/SignUp/components/PasswordInputField.js
+++ client/src/Page/User/SignUp/components/PasswordInputField.js
@@ -17,31 +17,31 @@ const PasswordLabel = styled(Label)`
 
 // 회원가입 페이지에서 비밀번호 인풋 영역
 const PasswordInputField = ({
-  password,
+  user,
+  isValidInput,
   passwordCheck,
-  isValidPassword,
   isValidPasswordCheck,
-  setPassword,
+  setUser,
+  setIsValidInput,
   setPasswordCheck,
-  setIsValidPassword,
   setIsValidPasswordCheck,
 }) => {
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
+  const [isPasswordEntered, setIsPasswordEntered] = useState(false);
+  const [isPasswordCheckEntered, setIsPasswordCheckEntered] = useState(false);
 
   useEffect(() => {
-    if (count1 === 0 && count2 === 0) return;
-    if (count1 > 0) setIsValidPassword(validateInput("비밀번호", password));
-    if (count2 > 0) setIsValidPasswordCheck(password === passwordCheck);
-  }, [password, passwordCheck]);
+    if (!isPasswordEntered && !isPasswordCheckEntered) return;
+    if (isPasswordEntered) setIsValidInput({ ...isValidInput, password: validateInput("password", user.password) });
+    if (isPasswordCheckEntered) setIsValidPasswordCheck(user.password === passwordCheck);
+  }, [user.password, passwordCheck]);
 
   const handlePasswordChange = (event) => {
-    if (count1 === 0) setCount1((prevState) => prevState + 1);
-    setPassword(event.target.value);
+    if (!isPasswordEntered) setIsPasswordEntered(true);
+    setUser({ ...user, password: event.target.value });
   };
 
   const handlePasswordCheckChange = (event) => {
-    if (count2 === 0) setCount2((prevState) => prevState + 1);
+    if (!isPasswordCheckEntered) setIsPasswordCheckEntered(true);
     setPasswordCheck(event.target.value);
   };
 
@@ -53,11 +53,11 @@ const PasswordInputField = ({
         id="password"
         autoComplete="on"
         placeholder="비밀번호를 입력해주세요."
-        value={password}
-        isValid={isValidPassword}
+        value={user.password}
+        isValid={isValidInput.password}
         onChange={handlePasswordChange}
       />
-      {isValidPassword === false && <Warning>8~20글자 사이 영문이나 숫자를 입력해주세요.</Warning>}
+      {isValidInput.password === false && <Warning>8~20글자 사이 영문이나 숫자를 입력해주세요.</Warning>}
       <Input
         type="password"
         id="passwordCheck"
