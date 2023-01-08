515c2841e9b84ef93cba5eda40a27a70bb49a605
LeeJoongWon <dlwnddnjs48@gmail.com>
2023-01-09 01:41:04 +0900

    yyyy.mm.dd 날짜를 yy/mm/dd로 변환해주는 함수

---
+++ client/src/Components/Function/DateOutput.js
@@ -0,0 +1,11 @@
+/**
+ * yyyy/mm/dd ~~ 로 이루어진 날짜데이터를 YY/MM/DD로 바꿔주는 함수입니다
+ * @param  {string} data 문자열을 넣어주세요
+ * @type {string}
+ * @return 20230102 => 23/01/02
+ */
+const DataOutput = (data) => {
+    return data ? `${data.slice(2, 4)}/${data.slice(4, 6)}/${data.slice(6, 8)}` : null;
+};
+
+export default DataOutput;
