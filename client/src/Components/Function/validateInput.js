3450b30ebfae9b897b0c0d43f4e198d918f69b28
yan9vvoojin <yan9vvoojin@naver.com>
2023-01-08 12:12:03 +0900

    refactor: 아이디, 비밀번호 등 유효성을 검사하는 함수 수정

--- client/src/Components/Function/validateInput.js
+++ client/src/Components/Function/validateInput.js
@@ -1,14 +1,8 @@
 // 아이디, 비밀번호, 닉네임, 이메일의 유효성을 검사하는 함수
-const validateInput = (label, value) => {
-  let regex;
-  if (label === "비밀번호") {
-    regex = /^[a-zA-Z0-9]{8,20}$/;
-  } else if (label === "이메일") {
-    regex = /^[a-zA-Z0-9.\-_]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]/;
-  } else {
-    regex = /^[a-zA-Z0-9]{5,16}$/;
-  }
-  return regex.test(value);
+const validateInput = (id, value) => {
+  if (id === "password") return /^[a-zA-Z0-9]{8,20}$/.test(value);
+  if (id === "email") return /^[a-zA-Z0-9.\-_]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]/.test(value);
+  return /^[a-zA-Z0-9]{5,16}$/.test(value);
 };
 
 export default validateInput;
