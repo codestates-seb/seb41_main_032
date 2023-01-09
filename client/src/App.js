c1f6c364a8cb9f8b6acd1d13a37bebb78b68c9f7
yan9vvoojin <yan9vvoojin@naver.com>
2023-01-09 12:28:17 +0900

    feat: 회원가입 페이지 라우팅 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,25 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Page/Home';
-import Stock from './Page/Stock';
-
-import styled from 'styled-components';
-
-import Header from './Page/Global/Header/index';
-import Sidebar from './Page/Global/Sidebar/index';
-
-const Div = styled.div`
-    display: flex;
-    flex-direction: row;
-`;
+import { Route, Routes } from "react-router-dom";
+import Header from "./Page/Global/Header/index";
+// import Sidebar from "./Page/Global/Sidebar/index";
+import Home from "./Page/Home";
+import Stock from "./Page/Stock";
+import SignUp from "./Page/User/SignUp/index";
 
 /**
  * @todo 사이드바와 컨텐츠 ui가 겹처서 보입니다 수정해주세요
  */
 function App() {
-    return (
-        <div className="App">
-            <Header />
-
-            {/* <Div>
-                <Sidebar />
-            </Div> */}
-
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/stock/:id" element={<Stock />} />
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Header />
+      {/* <Sidebar /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/stock/:id" element={<Stock />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
