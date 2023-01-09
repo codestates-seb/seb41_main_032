7710b3514ba534f0322283823b882ae645520f02
yan9vvoojin <yan9vvoojin@naver.com>
2023-01-09 17:51:35 +0900

    feat: 라우팅 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Components/Style/Layout";
 import Home from "./Page/Home";
 import Stock from "./Page/Stock";
-
-import Layout from "./Components/Style/Layout";
+import Login from "./Page/User/Login";
+import SignUp from "./Page/User/SignUp/index";
 
 function App() {
   return (
@@ -11,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/stock/:id" element={<Stock />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
     </Layout>
   );
