17981b7148ced351bf4781967fe241cd51627a59
LeeJoongWon <dlwnddnjs48@gmail.com>
2023-01-16 20:27:19 +0900

    css 백그라운드 linear-gradient 효과 추가

--- client/src/Page/Home/index.js
+++ client/src/Page/Home/index.js
@@ -3,16 +3,17 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import background from './img/background.png';
 
 const Main = styled.main`
-    background: url(${background}) fixed;
+    background: rgb(232, 203, 192);
+    background: linear-gradient(0deg, rgba(232, 203, 192, 1) 0%, rgba(99, 111, 164, 1) 100%);
     color: #fff;
     font-weight: 700px;
+    text-shadow: 1px 1px 1px black;
 
     header {
         text-align: center;
-        padding-top: 00px;
+        padding-top: 200px;
         h1 {
             font-size: 3em;
         }
@@ -69,32 +70,32 @@ const Home = () => {
                     시가총액 32조
                 </h1>
             </header>
-            <div class="cards">
-                <div class="card" data-aos="zoom-in" data-aos-delay="50" data-aos-duration="1000">
+            <div className="cards">
+                <div className="card" data-aos="zoom-in" data-aos-delay="50" data-aos-duration="1000">
                     주식이 처음이신가요?
                 </div>
-                <div class="card" data-aos="zoom-in" data-aos-delay="50" data-aos-duration="1000">
+                <div className="card" data-aos="zoom-in" data-aos-delay="50" data-aos-duration="1000">
                     주식으로 손해를 본적이 있으신가요?
                 </div>
-                <div class="card" data-aos="zoom-in" data-aos-delay="50" data-aos-duration="1000">
+                <div className="card" data-aos="zoom-in" data-aos-delay="50" data-aos-duration="1000">
                     주식매매가 두려우신가요?
                 </div>
-                <div class="card" data-aos="zoom-in" data-aos-delay="50" data-aos-duration="1000">
+                <div className="card" data-aos="zoom-in" data-aos-delay="50" data-aos-duration="1000">
                     그래서 준비했습니다
                 </div>
-                <div class="card" data-aos="zoom-in" data-aos-delay="50" data-aos-duration="1000">
+                <div className="card" data-aos="zoom-in" data-aos-delay="50" data-aos-duration="1000">
                     저희는 계좌계설 필요없이 회원가입만 하면
                 </div>
-                <div class="card" data-aos="zoom-in" data-aos-delay="50" data-aos-duration="1000">
+                <div className="card" data-aos="zoom-in" data-aos-delay="50" data-aos-duration="1000">
                     모의투자를 위한 1000만원을 지원해드립니다
                 </div>
-                <div class="card" data-aos="zoom-in" data-aos-delay="50" data-aos-duration="1000">
+                <div className="card" data-aos="zoom-in" data-aos-delay="50" data-aos-duration="1000">
                     가상의 돈으로 마음껏 매매를 해보세요
                 </div>
-                <div class="card" data-aos="zoom-in" data-aos-delay="50" data-aos-duration="1000">
-                    모의 투자로 자신감과 경험을 얻으시면 그때 진짜 실전투자를 하세요!
+                <div className="card" data-aos="zoom-in" data-aos-delay="50" data-aos-duration="1000">
+                    모의 투자로 자신감과 경험을 얻으세요
                 </div>
-                <div class="startBtn" data-aos="zoom-in" data-aos-delay="50" data-aos-duration="1000">
+                <div className="startBtn" data-aos="zoom-in" data-aos-delay="50" data-aos-duration="1000">
                     <Link to="/stock/top">
                         <button> 시작하기 </button>
                     </Link>
