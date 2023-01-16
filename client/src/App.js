65ad3a73a3678bb6f8e70d66d2259e9207aae083
LeeJoongWon <dlwnddnjs48@gmail.com>
2023-01-16 19:47:29 +0900

    서비스 소개페이지 home 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,31 @@
-import { Route, Routes } from "react-router-dom";
-import Layout from "./Components/Style/Layout";
-import StockTopList from "./Page/Stock/TopList";
-import StockDetail from "./Page/Stock/Detail";
-import StockAllList from "./Page/Stock/AllList";
-import Login from "./Page/User/Login";
-import SignUp from "./Page/User/SignUp/index";
-import Board from "./Page/Board/ArticleList";
-import NewArticle from "./Page/Board/NewArticle";
-import Article from "./Page/Board/Detail";
+import { Route, Routes } from 'react-router-dom';
+import Layout from './Components/Style/Layout';
+import StockTopList from './Page/Stock/TopList';
+import StockDetail from './Page/Stock/Detail';
+import StockAllList from './Page/Stock/AllList';
+import Login from './Page/User/Login';
+import SignUp from './Page/User/SignUp/index';
+import Board from './Page/Board/ArticleList';
+import NewArticle from './Page/Board/NewArticle';
+import Article from './Page/Board/Detail';
+import Home from './Page/Home';
 
 function App() {
-  return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<StockTopList />} />
-        <Route path="/stock/:id" element={<StockDetail />} />
-        <Route path="/stock/List" element={<StockAllList />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/board" element={<Board />} />
-        <Route path="/board/new" element={<NewArticle />} />
-        <Route path="/board/detail/:num" element={<Article />} />
-      </Routes>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/stock/:id" element={<StockDetail />} />
+                <Route path="/stock/top" element={<StockTopList />} />
+                <Route path="/stock/List" element={<StockAllList />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/board" element={<Board />} />
+                <Route path="/board/new" element={<NewArticle />} />
+                <Route path="/board/detail/:num" element={<Article />} />
+            </Routes>
+        </Layout>
+    );
 }
 
 export default App;
