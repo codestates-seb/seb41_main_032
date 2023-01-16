6fe339cdd7bff5e7cc9eb3a4fa1b162700350fb1
LeeJoongWon <dlwnddnjs48@gmail.com>
2023-01-16 19:47:56 +0900

    페이지 이동시 스크롤위치 초기화

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './Components/Function/ScrollToTop';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <ScrollToTop />
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>,
 );
