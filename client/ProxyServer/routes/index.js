be9b52167b2bbed1773e52e716ae2fd8cd487099
LeeJoongWon <dlwnddnjs48@gmail.com>
2023-01-19 13:38:23 +0900

    주석 추가

--- client/ProxyServer/routes/index.js
+++ client/ProxyServer/routes/index.js
@@ -1,3 +1,9 @@
+/**
+ * cors를 해결하기 위한 임시로 만든 프록시 서버입니다
+ * 문제가 있으시면 연락주세요
+ * @author 이중원
+ */
+
 const url = require('url');
 const express = require('express');
 const router = express.Router();
@@ -11,6 +17,7 @@ const API_APP_SECRET = process.env.API_APP_SECRET;
 
 // Init cache
 let cache = apicache.middleware;
+
 const tokenBody = {
     grant_type: 'client_credentials',
     appkey: APP_KEY,
@@ -66,7 +73,7 @@ router.get('/day', cache('1 minutes'), async (req, res, next) => {
         const apiRes = await needle('get', `${API_BASE_URL}/uapi/domestic-stock/v1/quotations/inquire-daily-itemchartprice?${params}`, null, stockHeaders);
         const data = apiRes.body;
 
-        // Log the request to the public API
+        // API에 대한 요청 기록
         if (process.env.NODE_ENV !== 'production') {
             // console.log(`REQUEST: ${API_BASE_URL}/uapi/domestic-stock/v1/quotations/inquire-daily-itemchartprice?${params}`);
         }
@@ -100,6 +107,34 @@ router.get('/investor', cache('1 minutes'), async (req, res, next) => {
         // Log the request to the public API
         if (process.env.NODE_ENV !== 'production') {
             // console.log(`REQUEST: ${API_BASE_URL}/uapi/domestic-stock/v1/quotations/inquire-investor?${params}`);
+        }
+
+        res.status(200).json(data);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// 네이버 뉴스 검색 api
+router.get('/search/news', cache('1 minutes'), async (req, res, next) => {
+    try {
+        const newsHeaders = {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Naver-Client-Id': 'DOzP6QlCZ1sB1weHWoqa',
+                'X-Naver-Client-Secret': 'hkRLPnoHV3',
+            },
+        };
+
+        const params = new URLSearchParams({
+            ...url.parse(req.url, true).query,
+        });
+        const apiRes = await needle('get', `https://openapi.naver.com/v1/search/news?${params}`, null, newsHeaders);
+        const data = apiRes.body;
+
+        // Log the request to the public API
+        if (process.env.NODE_ENV !== 'production') {
+            console.log(`REQUEST: https://openapi.naver.com/v1/search/news?${params}`);
         }
 
         res.status(200).json(data);
