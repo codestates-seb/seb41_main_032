c08e3f3ce5ba253385c21c52b640193aac9d79bd
LeeJoongWon <dlwnddnjs48@gmail.com>
2023-01-19 21:27:20 +0900

    커스텀 인풋

--- client/src/Components/Hook/useInput.js
+++ client/src/Components/Hook/useInput.js
@@ -1,6 +1,11 @@
 import { useState } from 'react';
 
-// 1번쨰 인자로 초기값, 2번쨰 인자로 버튼눌렀을때 동작하는 이벤트 함수
+/**  커스텀 인풋입니다
+ * @author 이중원
+ * @param {string} initialValue 초기값
+ * @param {string} onSubmit  버튼눌렀을때 동작하는 이벤트 함수
+ * @return [inputValue, setInputValue, handleChange, handleSubmit]
+ * */
 const useInput = (initialValue = '', onSubmit) => {
     const [inputValue, setInputValue] = useState(initialValue);
     const handleChange = (e) => {
