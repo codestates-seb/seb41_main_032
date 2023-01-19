47f272fbdd4062367dbfc2b9119da4f921323e0c
bangjaeyoung <qkdwo4613@gmail.com>
2023-01-20 00:19:51 +0900

    [Feat] 뉴스 기능 구현 완료

---
+++ server/src/main/java/mainproject/stocksite/domain/news/service/NewsService.java
@@ -0,0 +1,50 @@
+package mainproject.stocksite.domain.news.service;
+
+import mainproject.stocksite.domain.news.dto.NewsOptions;
+import mainproject.stocksite.domain.news.dto.NewsSecretInfo;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
+public class NewsService {
+
+    private final NewsSecretInfo newsSecretInfo;
+
+    public NewsService(NewsSecretInfo newsSecretInfo) {
+        this.newsSecretInfo = newsSecretInfo;
+    }
+
+    public ResponseEntity<Object> searchStockNews(NewsOptions newsOptions) {
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.set("Content-Type", "application/json");
+        requestHeaders.set("X-Naver-Client-Id", newsSecretInfo.getNewsClientId());
+        requestHeaders.set("X-Naver-Client-Secret", newsSecretInfo.getNewsClientSecret());
+        HttpEntity<String> requestMessage = new HttpEntity<>(requestHeaders);
+
+        String url = "https://openapi.naver.com/v1/search/news";
+
+        UriComponents uriBuilder = UriComponentsBuilder.fromHttpUrl(url)
+                .queryParam("query", newsOptions.getSearch())
+                .queryParam("display", newsOptions.getCount())
+                .queryParam("start", newsOptions.getStart())
+                .queryParam("sort", newsOptions.getSort())
+                .build();
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<Object> response = restTemplate.exchange(
+                uriBuilder.toString(),
+                HttpMethod.GET,
+                requestMessage,
+                Object.class
+        );
+
+        return new ResponseEntity<>(response.getBody(), response.getStatusCode());
+    }
+}
