e1ecdfcfeb6f0ce87f5688e2d80e022be9915246
LeeJoongWon <dlwnddnjs48@gmail.com>
2023-01-20 16:14:14 +0900

    즐겨찾기 기능 추가

--- client/src/Page/Stock/Detail/Components/StockBoard/Components/Info.js
+++ client/src/Page/Stock/Detail/Components/StockBoard/Components/Info.js
@@ -3,6 +3,9 @@ import { useParams } from 'react-router';
 import { RedTriangle, BlueTriangle } from '../../../../../../Components/Style/Stock';
 import commaGenerator from '../../../../../../Components/Function/commaGenerator';
 import { useLocation } from 'react-router';
+import DisableImg from '../../../../../../Components/Style/Stock/Img/Favorites/white.png';
+import ActivateImg from '../../../../../../Components/Style/Stock/Img/Favorites/gold.png';
+import { useState } from 'react';
 
 const PriceContainer = styled.div`
     margin-bottom: 30px;
@@ -29,15 +32,26 @@ const BluePrice = styled.span`
     color: #3a74ff;
 `;
 
+const FavoritesImg = styled.img`
+    width: 20px;
+    height: 20px;
+    margin-right: 5px;
+`;
+
 /** 주식의 이름,가격을 표시하는 컴포넌트입니다 */
 const Info = ({ todayInfo }) => {
     const params = useParams();
     const { state } = useLocation();
+    const [favorites, setFavorites] = useState(false);
+    const bookmarkHandler = () => {
+        setFavorites((current) => !current);
+    };
+
     return (
         <section>
             <p>{`한국증권 거래소 #${todayInfo.bstp_kor_isnm}`}</p>
             <h2>
-                {state.name} <span>{params.id}</span>
+                <FavoritesImg src={favorites ? ActivateImg : DisableImg} alt="Favorites" onClick={bookmarkHandler} /> {state.name} <span>{params.id}</span>
             </h2>
             <PriceContainer>
                 {todayInfo.prdy_vrss > 0 ? (
