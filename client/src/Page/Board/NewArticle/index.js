07c3633ce5ca10e993019ba4b0a971773b53ff79
FE41PIW <eatdrink0507@gmail.com>
2023-01-20 16:23:31 +0900

    Style : 리팩토링

--- client/src/Page/Board/NewArticle/index.js
+++ client/src/Page/Board/NewArticle/index.js
@@ -1,31 +1,34 @@
-import { useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import useInput from "../../../Components/Hook/useInput";
 
 const Div = styled.div`
   display: flex;
   flex-direction: column;
   min-height: 100%;
   padding: 30px 50px;
-  > input,
-  textarea {
-    margin-top: 30px;
-    border: solid 3px #eff5f5;
-    border-radius: 10px;
-  }
-  input {
-    height: 40px;
-    padding-left: 10px;
-  }
-  textarea {
-    padding: 10px;
-    height: 500px;
-    line-height: 1.5em;
-    resize: none;
-    font-size: 0.9em;
-    margin-bottom: 20px;
-  }
+`;
+
+const Input = styled.input`
+  border: ${(props) => props.borderColor} solid 2px;
+  border-radius: 10px;
+  height: 40px;
+  padding-left: 10px;
+  outline: none;
+  margin-top: 15px;
+`;
+
+const Textarea = styled.textarea`
+  border: ${(props) => props.borderColor} solid 2px;
+  border-radius: 10px;
+  padding: 10px;
+  height: 500px;
+  line-height: 1.5em;
+  resize: none;
+  font-size: 0.9em;
+  margin: 15px 0 20px 0;
+  outline: none;
 `;
 
 const Button = styled.button`
@@ -43,13 +46,7 @@ const Button = styled.button`
 
 const NewArticle = () => {
   // const navigate = useNavigate();
-  // 글 데이터
-  const [data, setData] = useState({
-    memberId: "",
-    title: "테스트",
-    content: "테스트",
-  });
-  const HandleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     // TODO : 서버 배포되면 기능 마저 구현
     // const url = `url`;
@@ -63,24 +60,43 @@ const NewArticle = () => {
     //   navigate("/board");
     //  });
   };
+  // 글 데이터
+  const [data, handleChange] = useInput({
+    memberId: "",
+    title: "",
+    content: "",
+  });
+  const handleDisable = !(data.title && data.content);
+
   return (
-    <form onSubmit={HandleSubmit}>
+    <form onSubmit={handleSubmit}>
       <Div>
         <h1>새 글 작성</h1>
-        <input
+        <Input
           type="text"
           id="title"
-          onChange={(e) => setData({ ...data, [e.target.id]: e.target.value })}
+          onChange={(e) =>
+            handleChange({ ...data, [e.target.id]: e.target.value })
+          }
           placeholder="제목을 입력해주세요(20자까지)"
           maxLength="20"
-        ></input>
-        <textarea
+          borderColor={data.title ? "#eff5f5" : "#0081C9"}
+        ></Input>
+
+        <Textarea
           type="text"
           id="content"
-          onChange={(e) => setData({ ...data, [e.target.id]: e.target.value })}
+          cols="20"
+          onChange={(e) =>
+            handleChange({ ...data, [e.target.id]: e.target.value })
+          }
           placeholder="본문을 입력해주세요"
-        ></textarea>
-        <Button type="submit">등록</Button>
+          borderColor={data.content ? "#eff5f5" : "#0081C9"}
+        ></Textarea>
+
+        <Button type="submit" disabled={handleDisable}>
+          등록
+        </Button>
       </Div>
     </form>
   );
