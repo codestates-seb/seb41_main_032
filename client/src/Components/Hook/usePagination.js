087d9df7a427a5321109135c9067a577dfba93ac
LeeJoongWon <dlwnddnjs48@gmail.com>
2023-01-24 02:12:14 +0900

    페이지 아이템 인자값받는 로직 추가

--- client/src/Components/Hook/usePagination.js
+++ client/src/Components/Hook/usePagination.js
@@ -20,10 +20,10 @@ const PageBtn = styled.li`
  * @param {Array} data2 배열을 넣어주세요
  * @returns [현재 페이지의 데이터, 현재 페이지의 페이지 번호, 페이지버튼 총 갯수, 페이지버튼, 페이지 뒤로가기, 페이지 앞으로가기, data, setData]
  */
-const usePagination = (data = []) => {
-    const [Data, setData] = useState(data);
+const usePagination = (_data = [], numItems = 10) => {
+    const [data, setData] = useState(_data);
     const [currentPage, setCurrentPage] = useState(1); //현재 페이지
-    const [itemsPerPage, setItemsPerPage] = useState(10); //아이템 갯수
+    const [itemsPerPage, setItemsPerPage] = useState(numItems); //아이템 갯수
     const [pageLimit, setPageLimit] = useState(5); // 페이지 버튼갯수
 
     // 페이지이동시 버튼값 변경을 위한 값
@@ -38,14 +38,14 @@ const usePagination = (data = []) => {
     /** 페이지버튼 총 갯수 */
     const pages = [];
     // Math.ceil -> 나눴을때 발생하는 소수점 처리
-    for (let i = 1; i <= Math.ceil(Data.length / itemsPerPage); i++) {
+    for (let i = 1; i <= Math.ceil(data.length / itemsPerPage); i++) {
         pages.push(i);
     }
 
     // 현재 페이지를 기준으로 데이터를 잘라서 보여줌 (itemsPerPage = 아이템 갯수 ,currentPage= 현재 페이지 )
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = Data.slice(indexOfFirstItem, indexOfLastItem);
+    const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
 
     /** 페이지 버튼 생성 */
     const renderPageNumbers = pages.map((num) => {
