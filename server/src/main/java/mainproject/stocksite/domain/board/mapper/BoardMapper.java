06351b6632872e65cf604adad501ceac514542b1
psh1379 <110961521+psh1379@users.noreply.github.com>
2023-01-25 21:54:24 +0900

    [fix] import문 누락 추가
    [fix] import문 누락 추가

--- server/src/main/java/mainproject/stocksite/domain/board/mapper/BoardMapper.java
+++ server/src/main/java/mainproject/stocksite/domain/board/mapper/BoardMapper.java
@@ -9,6 +9,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface BoardMapper {
