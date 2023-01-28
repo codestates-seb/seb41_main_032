cf14d4d81c8b2de3432ad3ed2d2865f0f75268f5
LeeJoongWon <dlwnddnjs48@gmail.com>
2023-01-29 01:00:02 +0900

    로그인정보 가져오기 로직

--- client/src/Components/Function/clearStorage.js
+++ client/src/Components/Function/clearStorage.js
@@ -1,7 +1,13 @@
+import { useRecoilState } from 'recoil';
+import { userInfo } from './userInfo';
+
 // 로컬 스토리지와 세션 스토리지에 저장된 데이터 제거하는 함수
-const clearStorage = () => {
-  localStorage.clear();
-  sessionStorage.clear();
+const ClearStorage = () => {
+    const [memberId, setMemberId] = useRecoilState(userInfo);
+    console.log('🚀  memberId', memberId);
+    setMemberId('');
+    localStorage.clear();
+    sessionStorage.clear();
 };
 
-export default clearStorage;
+export default ClearStorage;
