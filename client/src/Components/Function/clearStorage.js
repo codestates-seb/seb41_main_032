9bc9b9616150f58be2d1532baeec56ea4a3c4715
LeeJoongWon <dlwnddnjs48@gmail.com>
2023-01-29 10:48:02 +0900

    사용하지 않는 함수제거

--- client/src/Components/Function/clearStorage.js
+++ client/src/Components/Function/clearStorage.js
@@ -1,11 +1,5 @@
-import { useRecoilState } from 'recoil';
-import { userInfo } from './userInfo';
-
 // 로컬 스토리지와 세션 스토리지에 저장된 데이터 제거하는 함수
 const ClearStorage = () => {
-    const [memberId, setMemberId] = useRecoilState(userInfo);
-    console.log('🚀  memberId', memberId);
-    setMemberId('');
     localStorage.clear();
     sessionStorage.clear();
 };
