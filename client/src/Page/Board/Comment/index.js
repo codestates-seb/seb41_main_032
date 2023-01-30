2b429966d95b617fe0ebb656c803c3c18f55a1d8
LeeJoongWon <dlwnddnjs48@gmail.com>
2023-01-30 09:44:48 +0900

    댓글 자동업데이트

--- client/src/Page/Board/Comment/index.js
+++ client/src/Page/Board/Comment/index.js
@@ -1,88 +1,82 @@
-import styled from "styled-components";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import DeleteModal from "../Detail/deleteModal";
-import Portal from "../../../Components/Function/Portal";
+import styled from 'styled-components';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import DeleteModal from '../Detail/deleteModal';
+import Portal from '../../../Components/Function/Portal';
+import { useComment } from '../../../Components/API/ReactQueryContainer';
 
 const Box = styled.div`
-  display: flex;
-  flex-direction: column;
-  border-bottom: solid 1px gray;
-  padding: 15px 0 15px 0;
-  .user {
     display: flex;
-    font-size: 0.9em;
-    margin-bottom: 10px;
-    justify-content: space-between;
-  }
-  .comment {
-    white-space: pre-wrap;
-  }
+    flex-direction: column;
+    border-bottom: solid 1px gray;
+    padding: 15px 0 15px 0;
+    .user {
+        display: flex;
+        font-size: 0.9em;
+        margin-bottom: 10px;
+        justify-content: space-between;
+    }
+    .comment {
+        white-space: pre-wrap;
+    }
 `;
 const Li = styled.li`
-  list-style: none;
+    list-style: none;
 `;
 
 const Button = styled.button`
-  width: 40px;
-  height: 20px;
-  border-radius: 3px;
-  margin-left: 5px;
-  &:hover {
-    background-color: blue;
-    color: white;
-  }
+    width: 40px;
+    height: 20px;
+    border-radius: 3px;
+    margin-left: 5px;
+    &:hover {
+        background-color: blue;
+        color: white;
+    }
 `;
 
 const Comment = ({ memberId, boardId }) => {
-  const url = `${process.env.REACT_APP_API_URL}`;
-  const [commentData, setCommentData] = useState([]);
-  useEffect(() => {
-    axios.get(`${url}/comments?board=${boardId}`).then((res) => {
-      setCommentData(res.data);
-    });
-  }, []);
+    const url = `${process.env.REACT_APP_API_URL}`;
+    const [commentData, setCommentData] = useState([]);
+    const comment = useComment(boardId);
 
-  const [modalOn, setModalOn] = useState(false);
-  const handleModal = () => {
-    setModalOn(!modalOn);
-  };
+    useEffect(() => {
+        setCommentData(comment);
+    }, [comment]);
 
-  const deleteHandler = (e) => {
-    console.log("delete");
-    axios.delete(`${url}/comments/${e}`).then(() => {
-      window.location.reload();
-    });
-  };
+    const [modalOn, setModalOn] = useState(false);
+    const handleModal = () => {
+        setModalOn(!modalOn);
+    };
 
-  return (
-    <>
-      {commentData.map((el, index) => (
-        <Li key={index}>
-          <Box>
-            <div className="user">
-              {el.username}
-              {Number(memberId) === Number(el.memberId) ? (
-                <>
-                  <Portal>
-                    {modalOn && (
-                      <DeleteModal
-                        forDelete={() => deleteHandler(el.commentId)}
-                        onClose={handleModal}
-                      />
-                    )}
-                  </Portal>
-                  <Button onClick={() => handleModal()}>삭제</Button>
-                </>
-              ) : (
-                <></>
-              )}
-            </div>
-            <div className="comment">{el.content}</div>
-          </Box>
-        </Li>
-      ))}
-    </>
-  );
+    const deleteHandler = (e) => {
+        console.log('delete');
+        axios.delete(`${url}/comments/${e}`).then(() => {
+            window.location.reload();
+        });
+    };
+
+    return (
+        <>
+            {commentData?.map((el, index) => (
+                <Li key={index}>
+                    <Box>
+                        <div className="user">
+                            {el.username}
+                            {Number(memberId) === Number(el.memberId) ? (
+                                <>
+                                    <Portal>{modalOn && <DeleteModal forDelete={() => deleteHandler(el.commentId)} onClose={handleModal} />}</Portal>
+                                    <Button onClick={() => handleModal()}>삭제</Button>
+                                </>
+                            ) : (
+                                <></>
+                            )}
+                        </div>
+                        <div className="comment">{el.content}</div>
+                    </Box>
+                </Li>
+            ))}
+        </>
+    );
 };
 export default Comment;
