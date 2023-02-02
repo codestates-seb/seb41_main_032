de9dc379fa879f97065d266098dd3656a7d67a0e
LeeJoongWon <dlwnddnjs48@gmail.com>
2023-02-02 19:34:12 +0900

    닉네임 검사 변경

--- client/src/Components/Function/validateInput.js
+++ client/src/Components/Function/validateInput.js
@@ -1,8 +1,9 @@
 // 아이디, 비밀번호, 닉네임, 이메일의 유효성을 검사하는 함수
 const validateInput = (id, value) => {
-  if (id === "password") return /^[a-zA-Z0-9]{8,20}$/.test(value);
-  if (id === "email") return /^[a-zA-Z0-9.\-_]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]/.test(value);
-  return /^[a-zA-Z0-9]{5,16}$/.test(value);
+    if (id === 'password') return /^[a-zA-Z0-9]{8,20}$/.test(value);
+    if (id === 'email') return /^[a-zA-Z0-9.\-_]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]/.test(value);
+    if (id === 'nickname') return /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]{2,6}$/.test(value);
+    return /^[a-zA-Z0-9]{5,16}$/.test(value);
 };
 
 export default validateInput;
